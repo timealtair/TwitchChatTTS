0746cb91c701037950229115e721f00417646a76
timealtair <83885014+timealtair@users.noreply.github.com>
2024-10-19 00:31:37 +0400

    buf fix - incorrect last message pop

--- twitch_chat_tts.py
+++ twitch_chat_tts.py
@@ -5,8 +5,7 @@ from ai_streamer_lib import twitch_chat_reader
 from gtts_realtime import speak_text
 
 
-async def read_chat_loop(reader, settings, lang='ru',
-                         concatenate_func=lambda usr, msg: f'{usr}: {msg}'):
+async def read_chat_loop(reader, settings):
     while not settings.twitch_disable:
         if not reader:
             await asyncio.sleep(0.1)
@@ -17,9 +16,13 @@ async def read_chat_loop(reader, settings, lang='ru',
         else:
             usr, msg = reader.get_message_from_right_end()
 
-        txt = concatenate_func(usr, msg)
-        await speak_text(txt, lang)
-        await asyncio.sleep(settings.twitch_min_pause)
+        txt = settings.concatenate_func(usr, msg)
+        if not txt:
+            await asyncio.sleep(0.1)
+            continue
+
+        await speak_text(txt, settings.tts_lang)
+        await asyncio.sleep(settings.tts_min_pause)
 
 
 if __name__ == '__main__':
@@ -33,7 +36,10 @@ if __name__ == '__main__':
             self.twitch_censore_by = '*'
             self.twitch_replace_ban_word_dict = None
             self.twitch_disable = False
-            self.twitch_min_pause = 2
+
+            self.tts_min_pause = 2
+            self.tts_lang = 'ru'
+            self.concatenate_func = lambda usr, msg: f'{msg}'
 
     settings = CustomSetting()
 
@@ -42,10 +48,7 @@ if __name__ == '__main__':
     threads = []
     reader = twitch_chat_reader.TwitchChatReader(tokens_file, settings, stop_event, threads)
 
-    def concatenate_func(usr, msg):
-        return f'{msg}'
-
     try:
-        asyncio.run(read_chat_loop(reader, settings, 'ru', concatenate_func))
+        asyncio.run(read_chat_loop(reader, settings))
     except (KeyboardInterrupt, SystemExit):
         stop_event.set()
