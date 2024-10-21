e21f87577caa52f1f0dd8092982a27cd83060d88
timealtair <83885014+timealtair@users.noreply.github.com>
2024-10-21 19:17:13 +0400

    added readline3 to requirements.txt

--- ai_streamer_lib.py
+++ ai_streamer_lib.py
@@ -6,8 +6,10 @@ sys.path.append(AI_STREAMER_LIB_DIR)
 
 from twitch_chat_reader import TwitchChatReader
 from base_settings import BaseSettings
+from console_auto_completer import ConsoleAutoCompleter
 
 
 if __name__ == '__main__':
     print(dir(TwitchChatReader))
     print(dir(BaseSettings))
+    print(dir(ConsoleAutoCompleter))
