8ec0218a78979d951ca9801581e1817b50967649
timealtair <83885014+timealtair@users.noreply.github.com>
2024-10-25 10:20:49 +0400

    downgraded python version to 3.10

--- compile_win.cmd
+++ compile_win.cmd
@@ -9,29 +9,29 @@ mkdir temp
 cd temp
 
 :: Download Python embedded zip
-curl -o "python-3.13.0-embed-amd64.zip" "https://www.python.org/ftp/python/3.13.0/python-3.13.0-embed-amd64.zip"
+curl -o "python-3.10.0-embed-amd64.zip" "https://www.python.org/ftp/python/3.10.0/python-3.10.0-embed-amd64.zip"
 
 :: Unzip Python embedded distribution
-PowerShell -Command "Expand-Archive -Path 'python-3.13.0-embed-amd64.zip' -DestinationPath 'python-3.13.0-embed-amd64'"
+PowerShell -Command "Expand-Archive -Path 'python-3.10.0-embed-amd64.zip' -DestinationPath 'python-3.10.0-embed-amd64'"
 
 :: Create the ._pth file
 (
-    echo python313.zip
+    echo python310.zip
     echo .
     echo import site
-) > python-3.13.0-embed-amd64\python313._pth
+) > python-3.10.0-embed-amd64\python310._pth
 
 :: Download get-pip.py to install pip
-curl -o "python-3.13.0-embed-amd64\get-pip.py" "https://bootstrap.pypa.io/get-pip.py"
+curl -o "python-3.10.0-embed-amd64\get-pip.py" "https://bootstrap.pypa.io/get-pip.py"
 
 :: Install pip using the embedded Python
-python-3.13.0-embed-amd64\python.exe python-3.13.0-embed-amd64\get-pip.py
+python-3.10.0-embed-amd64\python.exe python-3.10.0-embed-amd64\get-pip.py
 
 :: Install PyInstaller using pip
-python-3.13.0-embed-amd64\Scripts\pip.exe install -U -r ..\requirements.txt
+python-3.10.0-embed-amd64\Scripts\pip.exe install -U -r ..\requirements.txt
 
 :: Create an executable for main.py
-python-3.13.0-embed-amd64\Scripts\PyInstaller.exe --onefile --name TwitchChatTTS.exe --icon=../source/twitch_chat_tts.ico ../source/main.py
+python-3.10.0-embed-amd64\Scripts\PyInstaller.exe --onefile --name TwitchChatTTS.exe --icon=../source/twitch_chat_tts.ico ../source/main.py
 
 :: Move TwitchChatTTS.exe to base directory
 
